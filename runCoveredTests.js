34c7918c4c8e2297713ebb6610b906ac26cc71b4
Alex Rea <git@area.io>
2017-01-05 17:42:17 +0000

    Have runCoveredTests.js use coverageMap.js

--- runCoveredTests.js
+++ runCoveredTests.js
@@ -3,10 +3,11 @@ var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
 var shell = require('shelljs');
 var SolidityCoder = require("web3/lib/solidity/coder.js");
-var coverage = {};
 var fs = require('fs');
 var path = require('path');
 var getInstrumentedVersion = require('./instrumentSolidity.js');
+var CoverageMap = require('./coverageMap.js');
+var coverage = new CoverageMap();
 
 var childprocess = require('child_process');
 
@@ -34,22 +35,7 @@ shell.ls('./../originalContracts/*.sol').forEach(function(file) {
         var instrumentedContractInfo = getInstrumentedVersion(contract, fileName, true);
         fs.writeFileSync('./../contracts/' + path.basename(file), instrumentedContractInfo.contract);
         var canonicalContractPath = path.resolve('./../originalContracts/' + path.basename(file));
-        coverage[canonicalContractPath] = { "l": {}, "path": canonicalContractPath, "s": {}, "b": {}, "f": {}, "fnMap": {}, "statementMap": {}, "branchMap": {} };
-        for (idx in instrumentedContractInfo.runnableLines) {
-            coverage[canonicalContractPath]["l"][instrumentedContractInfo.runnableLines[idx]] = 0;
-        }
-        coverage[canonicalContractPath].fnMap = instrumentedContractInfo.fnMap;
-        for (x=1; x<=Object.keys(instrumentedContractInfo.fnMap).length; x++ ){
-            coverage[canonicalContractPath]["f"][x] = 0;
-        }
-        coverage[canonicalContractPath].branchMap = instrumentedContractInfo.branchMap;
-        for (x=1; x<=Object.keys(instrumentedContractInfo.branchMap).length; x++ ){
-            coverage[canonicalContractPath]["b"][x] = [0,0];
-        }
-        coverage[canonicalContractPath].statementMap= instrumentedContractInfo.statementMap;
-        for (x=1; x<=Object.keys(instrumentedContractInfo.statementMap).length; x++ ){
-            coverage[canonicalContractPath]["s"][x] = 0;
-        }
+        coverage.addContract(instrumentedContractInfo, canonicalContractPath);
     }
 });
 shell.cp("./../originalContracts/Migrations.sol", "./../contracts/Migrations.sol");
@@ -58,30 +44,12 @@ shell.rm('./allFiredEvents'); //Delete previous results
 shell.exec('truffle test --network coverage');
 
 events = fs.readFileSync('./allFiredEvents').toString().split('\n')
-for (idx=0; idx < events.length-1; idx++){
-    //The limit here isn't a bug - there is an empty line at the end of this file, so we don't
-    //want to go to the very end of the array.
-    var event = JSON.parse(events[idx]);
-    if (event.topics.indexOf("b8995a65f405d9756b41a334f38d8ff0c93c4934e170d3c1429c3e7ca101014d") >= 0) {
-        var data = SolidityCoder.decodeParams(["string", "uint256"], event.data.replace("0x", ""));
-        var canonicalContractPath = path.resolve('./../originalContracts/' + path.basename(data[0]));
-        coverage[canonicalContractPath]["l"][data[1].toNumber()] += 1;
-    }else if(event.topics.indexOf("d4ce765fd23c5cc3660249353d61ecd18ca60549dd62cb9ca350a4244de7b87f")>=0){
-        var data = SolidityCoder.decodeParams(["string", "uint256"], event.data.replace("0x", ""));
-        var canonicalContractPath = path.resolve('./../originalContracts/' + path.basename(data[0]));
-        coverage[canonicalContractPath]["f"][data[1].toNumber()] += 1;
-    }else if(event.topics.indexOf("d4cf56ed5ba572684f02f889f12ac42d9583c8e3097802060e949bfbb3c1bff5")>=0){
-        var data = SolidityCoder.decodeParams(["string", "uint256", "uint256"], event.data.replace("0x", ""));
-        var canonicalContractPath = path.resolve('./../originalContracts/' + path.basename(data[0]));
-        coverage[canonicalContractPath]["b"][data[1].toNumber()][data[2].toNumber()] += 1;
-    }else if(event.topics.indexOf("b51abbff580b3a34bbc725f2dc6f736e9d4b45a41293fd0084ad865a31fde0c8")>=0){
-        var data = SolidityCoder.decodeParams(["string","uint256"], event.data.replace("0x", ""));
-        var canonicalContractPath = path.resolve('./../originalContracts/' + path.basename(data[0]));
-        coverage[canonicalContractPath]["s"][data[1].toNumber()]+= 1;
-    }
-}
+events.pop();
+//The pop here isn't a bug - there is an empty line at the end of this file, so we
+//don't want to include it as an event.
+coverage.generate(events, './../originalContracts/');
 
-fs.writeFileSync('./coverage.json', JSON.stringify(coverage));
+fs.writeFileSync('./coverage.json', JSON.stringify(coverage.coverage));
 
 shell.exec("./node_modules/istanbul/lib/cli.js report html")
 testrpcProcess.kill();
