c814f0a2b864e1f675fda6abf43566969e2777cc
cgewecke <christophergewecke@gmail.com>
2017-03-27 17:26:00 -0700

    Uport coverage

--- runCoveredTests.js
+++ runCoveredTests.js
@@ -17,23 +17,25 @@ if (!shell.test('-e', './node_modules/ethereumjs-vm/lib/opFns.js.orig')) {
   shell.exec('patch -b ./node_modules/ethereumjs-vm/lib/opFns.js ./hookIntoEvents.patch');
 }
 // Run the modified testrpc with large block limit
-const testrpcProcess = childprocess.exec('./node_modules/ethereumjs-testrpc/bin/testrpc --gasLimit 0xfffffffffffff --gasPrice 0x1');
+// commen this out and just run the same in another terminal. 
+const testrpcProcess = childprocess.exec('./node_modules/ethereumjs-testrpc/bin/testrpc --gasLimit 0xfffffffffffffffff --accounts 25');
 
 if (shell.test('-d', '../originalContracts')) {
+  shell.rm('-rf', './../contracts');
+  shell.mv('./../originalContracts', './../contracts');
   console.log('There is already an "originalContracts" directory in your truffle directory.\n' +
               'This is probably due to a previous solcover failure.\n' +
               'Please make sure the ./contracts/ directory contains your contracts (perhaps by copying them from originalContracts), ' +
               'and then delete the originalContracts directory.');
-  process.exit(1);
+  // process.exit(1);
 }
 
 shell.mv('./../contracts/', './../originalContracts/');
 shell.mkdir('./../contracts/');
 // For each contract in originalContracts, get the instrumented version
 shell.ls('./../originalContracts/**/*.sol').forEach(file => {
-  if (file !== 'originalContracts/Migrations.sol') {
+  if (file !== './../originalContracts/Migrations.sol') {
     const canonicalContractPath = path.resolve(file);
-
     console.log('instrumenting ', canonicalContractPath);
     const contract = fs.readFileSync(canonicalContractPath).toString();
     const instrumentedContractInfo = getInstrumentedVersion(contract, canonicalContractPath);
@@ -45,7 +47,12 @@ shell.ls('./../originalContracts/**/*.sol').forEach(file => {
 shell.cp('./../originalContracts/Migrations.sol', './../contracts/Migrations.sol');
 
 shell.rm('./allFiredEvents'); // Delete previous results
-shell.exec('truffle test --network test');
+console.log('running `truffle test`')
+
+//shell.exec('truffle test');
+//shell.exec('truffle test --network test');
+shell.exec('./node_modules/truffle/cli.js test --network test');
+
 
 const events = fs.readFileSync('./allFiredEvents').toString().split('\n');
 events.pop();
