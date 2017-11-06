14d7c6b51b477ab40320164b96d26ac0d778b211
cgewecke <christophergewecke@gmail.com>
2017-11-06 11:34:25 -0800

    Fix gas test, bump to testrpc-sc 6.0.4

--- test/sources/cli/Expensive.sol
+++ test/sources/cli/Expensive.sol
@@ -1,14 +1,13 @@
 // Cost to deploy Expensive:  0x4e042f
 // Block gas limit is: 0x47e7c4
-// Should throw out of gas on unmodified truffle 
+// Should throw out of gas on unmodified truffle
 // Should pass solcover truffle
 pragma solidity ^0.4.2;
 
 contract Expensive {
     mapping (uint => address) map;
-
     function Expensive(){
-        for(uint i = 0; i < 250; i++ ){
+        for(uint i = 0; i < 1000; i++ ){
             map[i] = address(this);
         }
     }
