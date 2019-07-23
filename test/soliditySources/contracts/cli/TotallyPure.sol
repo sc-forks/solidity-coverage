91c5202483c9b7c224d585b41e16e92f74a50f07
cgewecke <christophergewecke@gmail.com>
2019-07-23 11:33:30 -0700

    Initial draft: 0.7.0

---
+++ test/soliditySources/contracts/cli/TotallyPure.sol
@@ -0,0 +1,55 @@
+pragma solidity ^0.5.0;
+
+import "./../../externalSources/Face.sol";
+import "./../../externalSources/PureView.sol";
+import "./../../externalSources/CLibrary.sol";
+
+contract TotallyPure is PureView, Face {
+  uint onehundred = 99;
+
+  function usesThem() public view {
+    uint y = isPure(1,2);
+    uint z = isView();
+  }
+
+  function isPure(uint a, uint b) public pure returns (uint){
+    return a * b;
+  }
+
+  function isView() public view returns (uint){
+    return notpureview;
+  }
+
+  function isConstant() public view returns (uint){
+    return onehundred;
+  }
+
+  function bePure(uint a, uint b) public pure returns (uint) {
+    return a + b;
+  }
+
+  function beView() public view returns (uint){
+    return onehundred;
+  }
+
+  function usesLibrary() public view returns (uint){
+    return CLibrary.a();
+  }
+
+  function multiline(uint x,
+                     uint y)
+                     public
+                     view
+                     returns (uint)
+  {
+    return onehundred;
+  }
+
+  function stare(uint a, uint b) external {
+    uint z = a + b;
+  }
+
+  function cry() external {
+
+  }
+}
