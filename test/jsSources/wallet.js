fc3115138c54ef051bfb8ffda5295b7aebfa1a09
cgewecke <christophergewecke@gmail.com>
2019-07-23 11:33:30 -0700

    Initial draft: 0.7.0

---
+++ test/jsSources/wallet.js
@@ -0,0 +1,23 @@
+const Wallet = artifacts.require('./Wallet.sol');
+
+contract('Wallet', accounts => {
+  it('should should allow transfers and sends', async () => {
+    const walletA = await Wallet.new();
+    const walletB = await Wallet.new();
+
+    await walletA.sendTransaction({
+      value: web3.utils.toBN(500), from: accounts[0],
+    });
+
+    await walletA.sendPayment(50, walletB.address, {
+      from: accounts[0],
+    });
+
+    await walletA.transferPayment(50, walletB.address, {
+      from: accounts[0],
+    });
+
+    const balance = await walletB.getBalance();
+    assert.equal(balance.toNumber(), 100);
+  });
+});
---
+++ test/units/return.js
@@ -0,0 +1,14 @@
+/* eslint-env node, mocha */
+
+/*const solc = require('solc');
+const getInstrumentedVersion = require('./../lib/instrumentSolidity.js');
+const util = require('./util/util.js');
+
+describe('return statements', () => {
+  it('should compile after instrumenting function that returns true', () => {
+    const contract = util.getCode('return/return.sol');
+    const info = getInstrumentedVersion(contract, 'test.sol');
+    const output = JSON.parse(solc.compile(util.codeToCompilerInput(info.contract)));
+    util.report(output.errors);
+  });
+});*/
