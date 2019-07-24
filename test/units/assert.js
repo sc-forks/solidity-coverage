9332c9528326687c4e3119d3b6b53f5e4886073c
cgewecke <christophergewecke@gmail.com>
2019-07-23 19:12:34 -0700

    Remove ganache-core / misc cleanup

--- test/units/assert.js
+++ test/units/assert.js
@@ -1,10 +1,8 @@
 const assert = require('assert');
 const util = require('./../util/util.js');
 
-const ganache = require('ganache-core');
+const ganache = require('ganache-cli');
 const Coverage = require('./../../lib/coverage');
-const DataCollector = require('./../../lib/collector');
-const nodeUtil = require('util');
 
 describe('asserts and requires', () => {
   let coverage;
