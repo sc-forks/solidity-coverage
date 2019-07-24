40fbe927fd5ba1b726a5618c8938684bd642458c
cgewecke <christophergewecke@gmail.com>
2019-07-23 17:33:28 -0700

    Update function & expression tests

--- test/units/expressions.js
+++ test/units/expressions.js
@@ -1,24 +1,14 @@
-/* eslint-env node, mocha */
-
-/*const solc = require('solc');
-const getInstrumentedVersion = require('./../lib/instrumentSolidity.js');
-const util = require('./util/util.js');
-const path = require('path');
+const assert = require('assert');
+const util = require('./../util/util.js');
 
 describe('generic expressions', () => {
-  const filePath = path.resolve('./test.sol');
-
   it('should compile after instrumenting a single binary expression', () => {
-    const contract = util.getCode('expressions/single-binary-expression.sol');
-    const info = getInstrumentedVersion(contract, filePath);
-    const output = JSON.parse(solc.compile(util.codeToCompilerInput(info.contract)));
-    util.report(output.errors);
+    const info = util.instrumentAndCompile('expressions/single-binary-expression');
+    util.report(info.solcOutput.errors);
   });
 
   it('should compile after instrumenting a new expression', () => {
-    const contract = util.getCode('expressions/new-expression.sol');
-    const info = getInstrumentedVersion(contract, filePath);
-    const output = JSON.parse(solc.compile(util.codeToCompilerInput(info.contract)));
-    util.report(output.errors);
+    const info = util.instrumentAndCompile('expressions/new-expression');
+    util.report(info.solcOutput.errors);
   });
-});*/
+});
