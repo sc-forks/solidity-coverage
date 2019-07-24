6604b06a486cc814084f71e404c37cdd655a79bb
cgewecke <christophergewecke@gmail.com>
2019-07-23 17:46:49 -0700

    Update returns tests (move to generic expressions)

--- test/units/expressions.js
+++ test/units/expressions.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const util = require('./../util/util.js');
 
-describe('generic expressions', () => {
+describe('generic expressions / misc', () => {
   it('should compile after instrumenting a single binary expression', () => {
     const info = util.instrumentAndCompile('expressions/single-binary-expression');
     util.report(info.solcOutput.errors);
@@ -9,6 +9,11 @@ describe('generic expressions', () => {
 
   it('should compile after instrumenting a new expression', () => {
     const info = util.instrumentAndCompile('expressions/new-expression');
+    util.report(info.solcOutput.errors);
+  });
+
+  it('should compile after instrumenting function that returns true', () => {
+    const info = util.instrumentAndCompile('return/return');
     util.report(info.solcOutput.errors);
   });
 });
