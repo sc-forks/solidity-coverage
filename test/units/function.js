f341c64663ee5fdc54eddfa1a8f9effa5b105d48
cgewecke <christophergewecke@gmail.com>
2019-07-23 19:12:34 -0700

    Remove ganache-core / misc cleanup

--- test/units/function.js
+++ test/units/function.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const util = require('./../util/util.js');
 
-const ganache = require('ganache-core');
+const ganache = require('ganache-cli');
 const Coverage = require('./../../lib/coverage');
 
 describe('function declarations', () => {
--- test/units/if.js
+++ test/units/if.js
@@ -1,9 +1,8 @@
 const assert = require('assert');
 const util = require('./../util/util.js');
 
-const ganache = require('ganache-core');
+const ganache = require('ganache-cli');
 const Coverage = require('./../../lib/coverage');
-const DataCollector = require('./../../lib/collector');
 
 describe('if, else, and else if statements', () => {
   let coverage;
