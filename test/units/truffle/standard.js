ebf7edd5ff6ea8125233a1b8de951026512f7c4f
cgewecke <christophergewecke@gmail.com>
2019-09-19 23:46:01 -0700

    Improve coverage / misc test cleanup

--- test/units/truffle/standard.js
+++ test/units/truffle/standard.js
@@ -21,13 +21,12 @@ describe('Truffle Plugin: standard use cases', function() {
     mock.loggerOutput.val = '';
     solcoverConfig = {};
     truffleConfig = mock.getDefaultTruffleConfig();
+    verify.cleanInitialState();
   })
 
   afterEach(() => mock.clean());
 
   it('simple contract', async function(){
-    verify.cleanInitialState();
-
     mock.install('Simple', 'simple.js', solcoverConfig);
     await plugin(truffleConfig);
 
@@ -47,17 +46,7 @@ describe('Truffle Plugin: standard use cases', function() {
     );
   });
 
-  // Truffle test asserts balance is 777 ether
-  it('config with providerOptions', async function() {
-    solcoverConfig.providerOptions = { default_balance_ether: 777 }
-
-    mock.install('Simple', 'testrpc-options.js', solcoverConfig);
-    await plugin(truffleConfig);
-  });
-
-  it('large contract with many unbracketed statements (time check)', async function() {
-    verify.cleanInitialState();
-
+  it('with many unbracketed statements (time check)', async function() {
     truffleConfig.compilers.solc.version = "0.4.24";
 
     mock.install('Oraclize', 'oraclize.js', solcoverConfig, truffleConfig, true);
@@ -66,8 +55,7 @@ describe('Truffle Plugin: standard use cases', function() {
 
   // This project has three contract suites and uses .deployed() instances which
   // depend on truffle's migratons and the inter-test evm_revert / evm_snapshot mechanism.
-  it('project evm_reverts repeatedly', async function() {
-    verify.cleanInitialState();
+  it('with multiple migrations (evm_reverts repeatedly)', async function() {
     mock.installFullProject('multiple-migrations');
     await plugin(truffleConfig);
 
@@ -89,11 +77,124 @@ describe('Truffle Plugin: standard use cases', function() {
     verify.lineCoverage(expected);
   });
 
+  it('with relative path solidity imports', async function() {
+    mock.installFullProject('import-paths');
+    await plugin(truffleConfig);
+  });
+
+  it.only('uses libraries', async function() {
+    mock.installFullProject('libraries');
+    await plugin(truffleConfig);
+  });
+
+  it('uses native solidity tests', async function(){
+    mock.install('Simple', 'TestSimple.sol', solcoverConfig);
+
+    truffleConfig.logger = mock.testLogger;
+    await plugin(truffleConfig);
+
+    assert(
+      mock.loggerOutput.val.includes('native solidity tests'),
+      `Should warn it is skipping native solidity tests: ${mock.loggerOutput.val}`
+    );
+  });
+
+  it('uses inheritance', async function() {
+    mock.installDouble(
+      ['Proxy', 'Owned'],
+      'inheritance.js',
+      solcoverConfig
+    );
+
+    await plugin(truffleConfig);
+
+    verify.coverageGenerated(truffleConfig);
+
+    const output = mock.getOutput(truffleConfig);
+    const ownedPath = Object.keys(output)[0];
+    const proxyPath = Object.keys(output)[1];
+
+    assert(
+      output[ownedPath].fnMap['1'].name === 'constructor',
+      '"constructor" not covered'
+    );
+
+    assert(
+      output[proxyPath].fnMap['1'].name === 'isOwner',
+      '"isOwner" not covered'
+    );
+  });
+
+  it('only uses ".call"', async function(){
+    mock.install('OnlyCall', 'only-call.js', solcoverConfig);
+    await plugin(truffleConfig);
+
+    verify.coverageGenerated(truffleConfig);
+
+    const output = mock.getOutput(truffleConfig);
+    const path = Object.keys(output)[0];
+    assert(
+      output[path].fnMap['1'].name === 'addTwo',
+      'cov should map "addTwo"'
+    );
+  });
+
+  it('sends / transfers to instrumented fallback', async function(){
+    mock.install('Wallet', 'wallet.js', solcoverConfig);
+    await plugin(truffleConfig);
+
+    verify.coverageGenerated(truffleConfig);
+
+    const output = mock.getOutput(truffleConfig);
+    const path = Object.keys(output)[0];
+    assert(
+      output[path].fnMap['1'].name === 'transferPayment',
+      'cov should map "transferPayment"'
+    );
+  });
+
+  // Truffle test asserts deployment cost is greater than 20,000,000 gas
+  it('deployment cost > block gasLimit', async function() {
+    mock.install('Expensive', 'block-gas-limit.js', solcoverConfig);
+    await plugin(truffleConfig);
+  });
+
+  // Simple.sol with a failing assertion in a truffle test
+  it('unit tests failing', async function() {
+    mock.install('Simple', 'truffle-test-fail.js', solcoverConfig);
+
+    try {
+      await plugin(truffleConfig);
+      assert.fail()
+    } catch(err){
+      assert(err.message.includes('failed under coverage'));
+    }
+
+    verify.coverageGenerated(truffleConfig);
+
+    const output = mock.getOutput(truffleConfig);
+    const path = Object.keys(output)[0];
+
+    assert(output[path].fnMap['1'].name === 'test', 'cov missing "test"');
+    assert(output[path].fnMap['2'].name === 'getX', 'cov missing "getX"');
+  });
+
+  it('uses the fallback server', async function(){
+    truffleConfig.logger = mock.testLogger;
+    solcoverConfig.forceBackupServer = true;
+
+    mock.install('Simple', 'simple.js', solcoverConfig);
+    await plugin(truffleConfig);
+
+    assert(
+      mock.loggerOutput.val.includes("Using ganache-core-sc"),
+      `Should notify about backup server module: ${mock.loggerOutput.val}`
+    );
+  });
+
   // This project has [ @skipForCoverage ] tags in the test descriptions
   // at selected 'contract' and 'it' blocks.
-  it('uses solcoverjs mocha options', async function() {
-    verify.cleanInitialState();
-
+  it('config: mocha options', async function() {
     solcoverConfig.mocha = {
       grep: '@skipForCoverage',
       invert: true,
@@ -123,99 +224,23 @@ describe('Truffle Plugin: standard use cases', function() {
     verify.lineCoverage(expected);
   });
 
-  it('skips a folder', async function() {
-    verify.cleanInitialState();
-    mock.installFullProject('skipping');
-    await plugin(truffleConfig);
+  // Truffle test asserts balance is 777 ether
+  it('config: providerOptions', async function() {
+    solcoverConfig.providerOptions = { default_balance_ether: 777 }
 
-    const expected = [{
-     file: mock.pathToContract(truffleConfig, 'ContractA.sol'),
-     pct: 100
-    }];
-
-    const missing = [{
-     file: mock.pathToContract(truffleConfig, 'skipped-folder/ContractB.sol'),
-    }];
-
-    verify.lineCoverage(expected);
-    verify.coverageMissing(missing);
-  });
-
-  it('uses "onServerReady", "onTestsComplete", "onIstanbulComplete"', async function() {
-    verify.cleanInitialState();
-
-    truffleConfig.logger = mock.testLogger;
-    mock.installFullProject('test-files');
-
-    await plugin(truffleConfig);
-
-    assert(
-      mock.loggerOutput.val.includes('running onServerReady')     &&
-      mock.loggerOutput.val.includes('running onTestsComplete')   &&
-      mock.loggerOutput.val.includes('running onIstanbulComplete'),
-
-      `Should run "on" hooks : ${mock.loggerOutput.val}`
-    );
-  });
-
-  it('project with relative path solidity imports', async function() {
-    verify.cleanInitialState();
-    mock.installFullProject('import-paths');
+    mock.install('Simple', 'testrpc-options.js', solcoverConfig);
     await plugin(truffleConfig);
   });
 
-  it('project contains native solidity tests', async function(){
-    verify.cleanInitialState();
-
-    mock.install('Simple', 'TestSimple.sol', solcoverConfig);
-
-    truffleConfig.logger = mock.testLogger;
-    await plugin(truffleConfig);
-
-    assert(
-      mock.loggerOutput.val.includes('native solidity tests'),
-      `Should warn it is skipping native solidity tests: ${mock.loggerOutput.val}`
-    );
-  });
-
-  it('contract only uses ".call"', async function(){
-    verify.cleanInitialState();
-
-    mock.install('OnlyCall', 'only-call.js', solcoverConfig);
-    await plugin(truffleConfig);
-
-    verify.coverageGenerated(truffleConfig);
-
-    const output = mock.getOutput(truffleConfig);
-    const path = Object.keys(output)[0];
-    assert(
-      output[path].fnMap['1'].name === 'addTwo',
-      'cov should map "addTwo"'
-    );
-  });
-
-  it('contract sends / transfers to instrumented fallback', async function(){
-    verify.cleanInitialState();
-
-    mock.install('Wallet', 'wallet.js', solcoverConfig);
-    await plugin(truffleConfig);
-
-    verify.coverageGenerated(truffleConfig);
-
-    const output = mock.getOutput(truffleConfig);
-    const path = Object.keys(output)[0];
-    assert(
-      output[path].fnMap['1'].name === 'transferPayment',
-      'cov should map "transferPayment"'
-    );
-  });
-
-  it('contracts are skipped', async function() {
-    verify.cleanInitialState();
-
+  it('config: skipped file', async function() {
     solcoverConfig.skipFiles = ['Owned.sol'];
 
-    mock.installDouble(['Proxy', 'Owned'], 'inheritance.js', solcoverConfig);
+    mock.installDouble(
+      ['Proxy', 'Owned'],
+      'inheritance.js',
+      solcoverConfig
+    );
+
     await plugin(truffleConfig);
 
     verify.coverageGenerated(truffleConfig);
@@ -234,71 +259,35 @@ describe('Truffle Plugin: standard use cases', function() {
     );
   });
 
-  it('contract uses inheritance', async function() {
-    verify.cleanInitialState();
-
-    mock.installDouble(['Proxy', 'Owned'], 'inheritance.js', solcoverConfig);
+  it('config: skipped folder', async function() {
+    mock.installFullProject('skipping');
     await plugin(truffleConfig);
 
-    verify.coverageGenerated(truffleConfig);
+    const expected = [{
+     file: mock.pathToContract(truffleConfig, 'ContractA.sol'),
+     pct: 100
+    }];
 
-    const output = mock.getOutput(truffleConfig);
-    const ownedPath = Object.keys(output)[0];
-    const proxyPath = Object.keys(output)[1];
+    const missing = [{
+     file: mock.pathToContract(truffleConfig, 'skipped-folder/ContractB.sol'),
+    }];
 
-    assert(
-      output[ownedPath].fnMap['1'].name === 'constructor',
-      '"constructor" not covered'
-    );
-
-    assert(
-      output[proxyPath].fnMap['1'].name === 'isOwner',
-      '"isOwner" not covered'
-    );
+    verify.lineCoverage(expected);
+    verify.coverageMissing(missing);
   });
 
-  // Truffle test asserts deployment cost is greater than 20,000,000 gas
-  it('deployment cost > block gasLimit', async function() {
-    mock.install('Expensive', 'block-gas-limit.js', solcoverConfig);
-    await plugin(truffleConfig);
-  });
-
-  // Simple.sol with a failing assertion in a truffle test
-  it('truffle tests failing', async function() {
-    verify.cleanInitialState();
-
-    mock.install('Simple', 'truffle-test-fail.js', solcoverConfig);
-
-    try {
-      await plugin(truffleConfig);
-      assert.fail()
-    } catch(err){
-      assert(err.message.includes('failed under coverage'));
-    }
-
-    verify.coverageGenerated(truffleConfig);
-
-    const output = mock.getOutput(truffleConfig);
-    const path = Object.keys(output)[0];
-
-    assert(output[path].fnMap['1'].name === 'test', 'cov missing "test"');
-    assert(output[path].fnMap['2'].name === 'getX', 'cov missing "getX"');
-  });
-
-  it('uses the fallback server', async function(){
-    verify.cleanInitialState();
-
+  it('config: "onServerReady", "onTestsComplete", ...', async function() {
     truffleConfig.logger = mock.testLogger;
+    mock.installFullProject('test-files');
 
-    solcoverConfig = { forceBackupServer: true }
-
-    mock.install('Simple', 'simple.js', solcoverConfig);
     await plugin(truffleConfig);
 
     assert(
-      mock.loggerOutput.val.includes("Using ganache-core-sc"),
-      `Should notify about backup server module: ${mock.loggerOutput.val}`
-    );
+      mock.loggerOutput.val.includes('running onServerReady')     &&
+      mock.loggerOutput.val.includes('running onTestsComplete')   &&
+      mock.loggerOutput.val.includes('running onIstanbulComplete'),
 
+      `Should run "on" hooks : ${mock.loggerOutput.val}`
+    );
   });
 })
