b85e10288f708771ef72143c1313e2aa35fd36fb
cgewecke <christophergewecke@gmail.com>
2020-04-20 14:03:26 -0700

    Add logicalOR coverage for "require" conditions

--- test/units/or.js
+++ test/units/or.js
@@ -69,7 +69,7 @@ describe('logical OR branches', () => {
       5: 1,
     });
     assert.deepEqual(mapping[util.filePath].b, {
-      1: [1, 0],
+      1: [1, 0], 2: [1, 0]
     });
     assert.deepEqual(mapping[util.filePath].s, {
       1: 1,
@@ -92,7 +92,7 @@ describe('logical OR branches', () => {
       5: 2,
     });
     assert.deepEqual(mapping[util.filePath].b, {
-      1: [2, 0],
+      1: [2, 0], 2: [1, 1]
     });
     assert.deepEqual(mapping[util.filePath].s, {
       1: 2,
@@ -336,7 +336,7 @@ describe('logical OR branches', () => {
       5: 1, 9: 1
     });
     assert.deepEqual(mapping[util.filePath].b, {
-      1: [1, 0],
+      1: [1, 0], 2: [0, 1]
     });
     assert.deepEqual(mapping[util.filePath].s, {
       1: 1, 2: 1,
