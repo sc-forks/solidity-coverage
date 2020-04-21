0763bd6ee989796f705c4c8f9d610e0e6fa29c34
cgewecke <christophergewecke@gmail.com>
2020-04-20 17:59:19 -0700

    Fix parsing crash when return is ternary conditional product

--- test/units/expressions.js
+++ test/units/expressions.js
@@ -17,8 +17,13 @@ describe('generic expressions / misc', () => {
     util.report(info.solcOutput.errors);
   });
 
-  it('should compile after instrumenting function the returns void', () => {
+  it('should compile after instrumenting function that returns void', () => {
     const info = util.instrumentAndCompile('return/empty-return');
+    util.report(info.solcOutput.errors);
+  });
+
+  it('should compile after instrumenting function that returns via ternary conditional', () => {
+    const info = util.instrumentAndCompile('return/ternary-return');
     util.report(info.solcOutput.errors);
   });
 });
