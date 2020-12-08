83e24e53667c3d865a4a990130c00fe5396ad788
cgewecke <christophergewecke@gmail.com>
2020-12-07 20:10:59 -0800

    Add coverage for ternary conditionals (#587)

--- test/integration/projects/ternary-and-logical-or/.solcover.js
+++ test/integration/projects/ternary-and-logical-or/.solcover.js
@@ -4,5 +4,5 @@ const fn = (msg, config) => config.logger.log(msg);
 module.exports = {
   skipFiles: ['Migrations.sol'],
   silent: process.env.SILENT ? true : false,
-  istanbulReporter: ['json-summary', 'text'],
+  istanbulReporter: ['json-summary', 'text', 'html'],
 }
