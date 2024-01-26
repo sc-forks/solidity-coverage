70de9d452b0c8e2680816214d28c6baf4f791a44
cgewecke <christophergewecke@gmail.com>
2024-01-25 21:27:09 -0800

    Add test for file level `using for` statements (#831)

---
+++ test/integration/projects/solc-8/contracts/Library_solc8.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+library Library_solc8 {
+    uint constant x = 1;
+    function a() public view returns (uint) {
+        return x;
+    }
+}
