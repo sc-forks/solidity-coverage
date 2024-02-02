4cb98acea1c6ed5d373146ab9d21510c349974a9
cgewecke <christophergewecke@gmail.com>
2024-02-02 10:53:57 -0800

    Revert change to else-if-without-brackets.sol

--- test/sources/solidity/contracts/if/else-if-without-brackets.sol
+++ test/sources/solidity/contracts/if/else-if-without-brackets.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.8.0 <0.9.0;
 contract Test {
     function a(uint x) public {
         if (x == 1) {
-          x = 4;
+          revert();
         } else if (x == 50)
           x = 5;
     }
