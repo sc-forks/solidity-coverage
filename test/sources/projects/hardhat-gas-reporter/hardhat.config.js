db42f28747f61cfb65cde4f345a19777e3533d9b
cgewecke <christophergewecke@gmail.com>
2024-02-02 11:01:43 -0800

    Use solc 0.8.17 in integration tests

--- test/sources/projects/hardhat-gas-reporter/hardhat.config.js
+++ test/sources/projects/hardhat-gas-reporter/hardhat.config.js
@@ -4,7 +4,7 @@ require(__dirname + "/../plugins/nomiclabs.plugin");
 
 module.exports = {
   solidity: {
-    version: "0.7.3"
+    version: "0.8.17"
   },
   logger: process.env.SILENT ? { log: () => {} } : console,
 };
--- test/sources/projects/no-sources/hardhat.config.js
+++ test/sources/projects/no-sources/hardhat.config.js
@@ -3,7 +3,7 @@ require(__dirname + "/../plugins/nomiclabs.plugin");
 
 module.exports = {
   solidity: {
-    version: "0.7.3"
+    version: "0.8.17"
   },
   logger: process.env.SILENT ? { log: () => {} } : console,
 };
