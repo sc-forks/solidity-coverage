c3a64304cc5fa4a28db487366e47c8cb2141003c
cgewecke <christophergewecke@gmail.com>
2024-02-09 13:37:12 -0800

    Remove log lines

--- test/util/util.js
+++ test/util/util.js
@@ -119,9 +119,6 @@ function report(output=[]) {
 async function bootstrapCoverage(file, api, provider){
   const info = instrumentAndCompile(file, api);
 
-  const { inspect } = require("util");
-  //console.log('solc: ' + inspect(info.solcOutput));
-
   // Need to define a gasLimit for contract calls because otherwise ethers will estimateGas
   // and cause duplicate hits for everything
   info.gas = { gasLimit: 2_000_000 }
