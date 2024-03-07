93faab639f8a326b18c21785d46a37b1257e9258
cgewecke <christophergewecke@gmail.com>
2024-03-07 14:24:48 -0800

    Try Node 20 (fix OZ #1)

--- scripts/zeppelin.sh
+++ scripts/zeppelin.sh
@@ -8,7 +8,7 @@ set -o errexit
 # Get rid of any caches
 sudo rm -rf node_modules
 echo "NVM CURRENT >>>>>" && nvm current
-nvm use 18
+nvm use 20
 
 # Use PR env variables (for forks) or fallback on local if PR not available
 SED_REGEX="s/git@github.com:/https:\/\/github.com\//"
