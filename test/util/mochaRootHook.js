011ee40ecf1494b23c069750c17dfe6006d295e1
cgewecke <christophergewecke@gmail.com>
2024-04-04 11:11:47 -0700

    Update hardhat dev dep to 2.22.2 (EDR) (#881)

--- test/util/mochaRootHook.js
+++ test/util/mochaRootHook.js
@@ -2,9 +2,9 @@ const { createProvider } = require("hardhat/internal/core/providers/construction
 const { resolveConfig } = require("hardhat/internal/core/config/config-resolution");
 const { HARDHAT_NETWORK_NAME } = require("hardhat/plugins")
 
-// Creates a shared, minimal HH provider for use in the unit tests
+// Creates a minimal HH provider for use in the unit tests
 exports.mochaHooks = {
-  beforeAll: async function() {
+  beforeEach: async function() {
     const config = await resolveConfig("./", {});
     this.provider = await createProvider(
       config,
